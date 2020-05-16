652f13d837119540b0d54f5fc547ad0e447c1c77
David Mente <davidmente@hotmail.de>
2020-05-16 14:47:00 +0200

    minor changes

--- src/client/src/components/Navbar.tsx
+++ src/client/src/components/Navbar.tsx
@@ -5,10 +5,7 @@ const Navbar: FunctionComponent = () =>
   <nav className="navbar has-text-weight-bold has-text-white" role="navigation"
        aria-label="main navigation">
     <div className="navbar-brand">
-      <div className="navbar-item">David's Cards Against Humanity App</div>
-    </div>
-    <div className="navbar-start">
-      <Link className="navbar-item" to={'/'}>Home</Link>
+      <Link className="navbar-item navbar-custom-link" to={'/'}>David's Cards Against Humanity App</Link>
     </div>
   </nav>;
 
